a350c57ac6746b1e42e3c704fe7f2e47e1cc03ab
sueshin <sueshin@student.42seoul.kr>
2022-08-30 16:53:37 +0900

    .

--- _posts/C++/test.cpp
+++ _posts/C++/test.cpp
@@ -1,20 +1,23 @@
-// choicess.cpp
+// nested.cpp
 
 #include <iostream>
-#include <array>
 
 int main()
 {
 	using namespace std;
-	array<double, 3> run;
 
-	cout << "첫번째 달리기 결과를 입력하시오 : ";
-	cin >> run[0];
-	cout << "두번째 달리기 결과를 입력하시오 : ";
-	cin >> run[1];
-	cout << "세번째 달리기 결과를 입력하시오 : ";
-	cin >> run[2];
-
-	cout << "총 세번의 달리기의 평균은 " << (run[0] + run[1] + run[2]) / 3 << "입니다.\n";
+	cout << "출력할 행 수를 입력하십시오 : ";
+	int num;
+	cin >> num;
+	for (int i = 1; i <= num; i++)
+	{
+		for (int j = i; j < num; j++)
+			cout << ".";
+		for (int j = 0; j < i; j++)
+			cout << "*";
+		cout << endl;
+	}
 	return 0;
 }
+
+//cout << "문자들을 입력하시오; 끝내려면 #을 입력하시오 : \n";
