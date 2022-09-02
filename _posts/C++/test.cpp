9ee9ea4d3068f4e2db2143ba15fe5b364a5305ea
sueshin <sueshin@student.42seoul.kr>
2022-09-02 22:27:59 +0900

    .

--- _posts/C++/test.cpp
+++ _posts/C++/test.cpp
@@ -1,47 +1,32 @@
-// sumafile.cpp
+// strgback.cpp
 
 #include <iostream>
-#include <string>
-#include <fstream>
-struct donation
-{
-	std::string name;
-	double money;
-};
+char * buildstr(char c, int n);
 
 int main()
 {
 	using namespace std;
-	char filename[100];
-	ifstream inFile;
+	int times;
+	char ch;
 
-
-	cout << "기부 정보를 파일로부터 가져옵니다. 파일 이름을 입력해주세요 : ";
-	cin >> filename;
-	inFile.open(filename);
-	int person;
-	inFile >> person;
-	donation * dn = new donation [person];
-
-	for (int i = 0; i < person; i++)
-	{
-		getline(inFile, dn[i].name);
-		getline(inFile, dn[i].name);
-		cout << dn[i].name << endl;
-		inFile >> dn[i].money;
-	}
-
-	for (int i = 0; i < person; i++)
-	{
-		if (dn[i].money >= 10000)
-			cout << "고액 기부자 " << dn[i].name << " : " << dn[i].money << "$ 기부\n";
-	}
-	for (int i = 0; i < person; i++)
-	{
-		if (dn[i].money < 10000)
-			cout << "소액 기부자 " << dn[i].name << " : " << dn[i].money << "$ 기부\n";
-	}
-	if (person == 0)
-		cout << "기부자가 없습니다.\n";
+	cout << "문자 하나를 입력하십시오 : ";
+	cin >> ch;
+	cout << "정수 하나를 입력하십시오 : ";
+	cin >> times;
+	char *ps = buildstr(ch, times);
+	cout << ps << endl;
+	delete [] ps;
+	ps = buildstr('+', 20);
+	cout << ps << "-DONE-" << ps << endl;
+	delete [] ps;
 	return 0;
+}
+
+char * buildstr(char c, int n)
+{
+	char * pstr = new char[n + 1];
+	pstr[n] = '\0';
+	while (n-- > 0)
+		pstr[n] = c;
+	return pstr;
 }
