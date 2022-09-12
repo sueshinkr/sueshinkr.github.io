e39dc4b2b40e49306b2e57b819e9514fbcacf715
sueshin <sueshin@student.42seoul.kr>
2022-09-12 21:10:25 +0900

    .

---
+++ _posts/C++/namesp.cpp
@@ -0,0 +1,44 @@
+// namesp.cpp
+
+#include <iostream>
+#include "namesp.h"
+
+namespace pers
+{
+	using std::cout;
+	using std::cin;
+	void getPerson(Person & rp)
+	{
+		cout << "이름을 입력하십시오 : ";
+		cin >> rp.fname;
+		cout << "성씨를 입력하십시오 : ";
+		cin >> rp.lname;
+	}
+	void showPerson(const Person & rp)
+	{
+		std::cout << rp.lname << ", " << rp.fname;
+	}
+}
+
+namespace debts
+{
+	void getDebt(Debt & rd)
+	{
+		getPerson(rd.name);
+		std::cout << "부채를 입력하십시오 : ";
+		std::cin >> rd.amount;
+	}
+	void showDebt(const Debt & rd)
+	{
+		pers::showPerson(rd.name);
+		std::cout << " : $" << rd.amount << std::endl;
+	}
+	
+	double sumDebts(const Debt ar[], int n)
+	{
+		double total = 0;
+		for (int i = 0; i < n; i++)
+			total += ar[i].amount;
+		return total;
+	}
+}
--- _posts/C++/test.cpp
+++ _posts/C++/test.cpp
@@ -1,42 +1,59 @@
-// autoscp.cpp
+// newplace.cpp
 
 #include <iostream>
-void oil(int x);
+#include <new>
+
+const int BUF = 512;
+const int N = 5;
+char buffer[BUF];
 
 int main()
 {
 	using namespace std;
 
-	int texas = 31;
-	int year = 2011;
-	cout << "main()에서, texas = " << texas << ", &texas = ";
-	cout << &texas << endl;
-	cout << "main()에서, year = " << year << ", &year = ";
-	cout << &year << endl;
-	oil(texas);
-	cout << "main()에서, texas = " << texas << ", &texas = ";
-	cout << &texas << endl;
-	cout << "main()에서, year = " << year << ", &year = ";
-	cout << &year << endl;
-	return 0;
-}
+	double *pd1, *pd2;
+	int i;
+	cout << "new와 위치 지정 new의 첫번째 호출 : \n";
+	pd1 = new double[N];
+	pd2 = new (buffer) double[N];
 
-void oil(int x)
-{
-	using namespace std;
-	int texas = 5;
-
-	cout << "oil()에서, texas = " << texas << ", &texas = ";
-	cout << &texas << endl;
-	cout << "oil()에서, x = " << x << ", &x = ";
-	cout << &x << endl;
+	for (i = 0; i < N; i++)
+		pd2[i] = pd1[i] = 1000 + 20.0 * i;
+	cout << "메모리 주소 : \n" << pd1 << " : 힙;	"
+		 << (void *) buffer << " : 정적" << endl;
+	cout << "메모리 내용 : \n";
+	for (i = 0; i < N; i++)
 	{
-		int texas = 113;
-		cout << "블록에서, texas = " << texas << ", &texas = ";
-		cout << &texas << endl;
-		cout << "블록에서, x = " << x << ", &x = ";
-		cout << &x << endl;
+		cout << &pd1[i] << "에 " << pd1[i] << ";	";
+		cout << &pd2[i] << "에 " << pd2[i] << endl;
 	}
-	cout << "블록을 통과한 후, texas = " << texas;
-	cout << ", &texas = " << &texas << endl;
+
+	cout << "\nnew와 위치 지정 new의 두번째 호출 : \n";
+	double *pd3, *pd4;
+	pd3 = new double[N];
+	pd4 = new (buffer) double[N];
+	for (i = 0; i < N; i++)
+		pd4[i] = pd3[i] = 1000 + 40.0 * i;
+	cout << "메모리 내용 : \n";
+	for (i = 0; i < N; i++)
+	{
+		cout << &pd3[i] << "에 " << pd3[i] << ";	";
+		cout << &pd4[i] << "에 " << pd4[i] << endl;
+	}
+
+	cout << "\nnew와 위치 지정 new의 세번째 호출 : \n";
+	delete [] pd1;
+	pd1 = new double[N];
+	pd2 = new (buffer + N * sizeof(double)) double[N];
+	for (i = 0; i < N; i++)
+		pd2[i] = pd1[i] = 1000 + 60.0 * i;
+	cout << "메모리 내용 : \n";
+	for (i = 0; i < N; i++)
+	{
+		cout << &pd1[i] << "에 " << pd1[i] << ";	";
+		cout << &pd2[i] << "에 " << pd2[i] << endl;
+	}
+	delete [] pd1;
+	delete [] pd3;
+	return 0;
 }
