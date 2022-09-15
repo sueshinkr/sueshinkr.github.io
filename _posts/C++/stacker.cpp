1d25158abaf1bf4adb142bdb28ece19586a7e548
sueshin <sueshin@student.42seoul.kr>
2022-09-15 19:26:30 +0900

    .

---
+++ _posts/C++/stacker.cpp
@@ -0,0 +1,49 @@
+// stacker.cpp
+
+#include <iostream>
+#include <cctype>
+#include "stack.h"
+
+int main()
+{
+	using namespace std;
+	Stack st;
+	char ch;
+	unsigned long po;
+
+	cout << "주문서를 추가하려면 A, 주문서를 처리하려면 P, \n"
+		 << "종료하려면 Q를 입력하십시오.\n";
+	while (cin >> ch && toupper(ch) != 'Q')
+	{
+		while (cin.get() != '\n')
+			continue;
+		if (!isalpha(ch))
+		{
+			cout << '\a';
+			continue;
+		}
+		switch (ch)
+		{
+			case 'A':
+			case 'a': cout << "추가할 주문서의 번호를 입력하십시오 : ";
+					  cin >> po;
+					  if (st.isfull())
+					  	cout << "스택이 가득 차 있습니다.\n";
+					  else
+					  	st.push(po);
+					  break;
+			case 'P':
+			case 'p': if (st.isempty())
+					  	cout << "스택이 비어있습니다.\n";
+					  else {
+						st.pop(po);
+						cout << "#" << po << " 주문서를 처리했습니다.\n";
+					  }
+					  break;
+		}
+		cout << "주문서를 추가하려면 A, 주문서를 처리하려면 P,\n"
+			 << "종료하려면 Q를 입력하십시오.\n";
+	}
+	cout << "프로그램을 종료합니다.\n";
+	return 0;
+}
--- _posts/C++/test.cpp
+++ _posts/C++/test.cpp
@@ -1,45 +1,69 @@
-// static.cpp
+// stock00.cpp - version 00
 
 #include <iostream>
+#include "stock00.h"
 
-struct chaff
+void Stock::acquire(const std::string & co, long n, double pr)
 {
-	char dross[20];
-	int slag;
-};
+	company = co;
+	if (n < 0)
+	{
+		std::cout << "주식 수는 음수가 될 수 없으므로, "
+				  << company << " shares를 0으로 설정합니다.\n";
+		shares = 0;
+	}
+	else
+		shares = n;
+	share_val = pr;
+	set_tot();
+}
 
-const int N = 2;
-const int BUF = 512;
-char buffer[BUF];
-
-int main()
+void Stock::buy(long num, double price)
 {
-	using namespace std;
-
-	chaff *arr;
-
-	arr = new (buffer) chaff[N];
-
-	char temp[20];
-	for (int i = 0; i < N; i++)
+	if (num < 0)
 	{
-		cout << "##" << i + 1 << "번째##\n";
-		cout << "dross 입력 : ";
-		cin.getline(temp, 19);
-		strcpy(arr[i].dross, temp);
-
-		cout << "slag 입력 : ";
-		cin >> arr[i].slag;
-		cin.get();
+		std::cout << "매입 주식 수는 음수가 될 수 없으므로, "
+				  << "거래가 취소되었습니다.\n";
 	}
-
-	cout << endl;
-
-	for (int i = 0; i < N; i++)
+	else
 	{
-		cout << "##" << i + 1 << "번째##\n";
-		cout << "dross : " << arr[i].dross << endl;
-		cout << "slag : " << arr[i].slag << endl;
+		shares += num;
+		share_val = price;
+		set_tot();
 	}
-	return 0;
+}
+
+void Stock::sell(long num, double price
+{
+	using std::cout;
+	if (num < 0)
+	{
+		cout << "매입 주식 수는 음수가 될 수 없으므로, "
+			 << "거래가 취소되었습니다.\n";
+	}
+	else if (num > shares)
+	{
+		cout << "보유 주식보다 많은 주식을 매도할 수 없으므로, "
+			 << "거래가 취소되었습니다.\n";
+	}
+	else
+	{
+		shares -= num;
+		share_val = price;
+		set_tot();
+	}
+}
+
+void Stock::update(double price)
+{
+	share_val = price;
+	set_tot();
+}
+
+void Stock::show()
+{
+	std::cout << "회사명 : " << company
+			  << " 주식 수 : " << shares << '\n'
+			  << " 주가 : $" << share_val
+			  << " 주식 총 가치 : $" << total_val << '\n';
 }
