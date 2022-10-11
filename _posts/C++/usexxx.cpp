372cae9f90a88d1a13f374298a88849910ac640a
sueshin <sueshin@student.42seoul.kr>
2022-10-11 20:39:37 +0900

    .

--- _posts/C++/usexxx.cpp
+++ _posts/C++/usexxx.cpp
@@ -1,28 +1,88 @@
-// str2.cpp
+// vect3.cpp
 
-//#include "xxx.h"
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+
+struct Review {
+	std::string title;
+	int rating;
+};
+
+bool operator<(const Review & r1, const Review & r2);
+bool worseThan(const Review & r1, const Review & r2);
+bool FillReview(Review & rr);
+void ShowReview(const Review & rr);
 
 int main()
 {
 	using namespace std;
-	string empty;
-	string small = "bit";
-	string larger = "Elephants are a girl's mest friend";
-	
-	cout << "크기 : \n";
-	cout << "\tempty : " << empty.size() << endl;
-	cout << "\tsmall : " << small.size() << endl;
-	cout << "\tlarger : " << larger.size() << endl;
-	
-	cout << "용량 : \n";
-	cout << "\tempty : " << empty.capacity() << endl;
-	cout << "\tsmall : " << small.capacity() << endl;
-	cout << "\tlarger : " << larger.capacity() << endl;
-	
-	empty.reserve(50);
-	cout << "empty.reserve(50) 이후 용량 : "
-		 << empty.capacity() << endl;
+
+	vector<Review> books;
+	Review temp;
+
+	while (FillReview(temp))
+		books.push_back(temp);
+
+	if (books.size() > 0)
+	{
+		cout << "감사합니다. 당신은 다음과 같이 "
+			 << books.size() << "개의 책 등급을 입력하셨습니다.\n"
+			 << "등급\t제목\n";
+		for_each(books.begin(), books.end(), ShowReview);
+
+		sort(books.begin(), books.end());
+		cout << "책 제목을 기준으로 정렬 : \n등급\t제목\n";
+		for_each(books.begin(), books.end(), ShowReview);
+
+		sort(books.begin(), books.end(), worseThan);
+		cout << "책 등급을 기준으로 정렬 : \n등급\t제목\n";
+		for_each(books.begin(), books.end(), ShowReview);
+
+		random_shuffle(books.begin(), books.end());
+		cout << "무작위 순서로 다시 배치 : \n등급\t제목\n";
+		for_each(books.begin(), books.end(), ShowReview);
+	}
+	else
+		cout << "프로그램을 종료합니다.\n";
 	return 0;
+}
+
+bool operator<(const Review & r1, const Review & r2)
+{
+	if (r1.title < r2.title)
+		return true;
+	else if (r1.title == r2.title && r1.rating < r2.rating)
+		return true;
+	else
+		return false;
+}
+
+bool worseThan(const Review & r1, const Review & r2)
+{
+	if (r1.rating < r2.rating)
+		return true;
+	else
+		return false;
+}
+
+bool FillReview(Review & rr)
+{
+	std::cout << "책 제목을 입력하십시오(끝내려면 quit를 입력) : ";
+	std::getline(std::cin, rr.title);
+	if (rr.title == "quit")
+		return false;
+	std::cout << "책 등급(0-10)을 입력하십시오 : ";
+	std::cin >> rr.rating;
+	if (!std::cin)
+		return false;
+	while (std::cin.get() != '\n')
+		continue;
+	return true;
+}
+
+void ShowReview(const Review & rr)
+{
+	std::cout << rr.rating << "\t" << rr.title << std::endl;
 }
