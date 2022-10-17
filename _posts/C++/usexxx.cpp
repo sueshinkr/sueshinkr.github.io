6fca1b671f7e03a4b5aea6d41da823a8ef5e3d3b
sueshin <sueshin@student.42seoul.kr>
2022-10-17 16:24:56 +0900

    .

--- _posts/C++/usexxx.cpp
+++ _posts/C++/usexxx.cpp
@@ -1,39 +1,26 @@
-// Pat&Mat.cpp
+// iomanip.cpp
 
 #include <iostream>
-#include <list>
-using namespace std;
+#include <iomanip>
+#include <cmath>
 
 int main()
 {
-	list<string> Pat;
-	list<string> Mat;
-	list<string> PatandMat;
+	using namespace std;
+	cout << fixed << right;
 
-	cout << "Mat의 친구들을 입력하세요 (끝내려면 quit) : \n";
-	string temp;
-	while (cin >> temp && temp != "quit")
-		Mat.push_back(temp);
-	Mat.sort();
-	cout << "Mat의 친구들 목록입니다.\n";
-	for (list<string>::iterator i = Mat.begin(); i != Mat.end(); ++i)
-		cout << (*i) << endl;
-
-	cout << "Pat의 친구들을 입력하세요 (끝내려면 quit) : \n";
-	while (cin >> temp && temp != "quit")
-		Pat.push_back(temp);
-	Pat.sort();
-	cout << "Pat의 친구들 목록입니다.\n";
-	for (list<string>::iterator i = Pat.begin(); i != Pat.end(); ++i)
-		cout << (*i) << endl;
-
-	PatandMat = Mat;
-	PatandMat.merge(Pat);
-	PatandMat.unique();
-
-	cout << "Pat&Mat의 친구들 목록입니다.\n";
-	for (list<string>::iterator i = PatandMat.begin(); i != PatandMat.end(); ++i)
-		cout << (*i) << endl;
+	cout << setw(6) << "N" << setw(14) << "제곱근"
+		 << setw(15) << "네제곱근\n";
+	
+	double root;
+	for (int n = 10; n <= 100; n += 10)
+	{
+		root = sqrt(double(n));
+		cout << setw(6) << setfill('.') << n << setfill(' ')
+			 << setw(12) << setprecision(3) << root
+			 << setw(14) << setprecision(4) << sqrt(root)
+			 << endl;
+	}
 
 	return 0;
 }
