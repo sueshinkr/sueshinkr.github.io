542075e0299ff508b37a5c6a05ecd99188d5da33
sueshin <sueshin@student.42seoul.kr>
2022-10-19 15:16:56 +0900

    .

--- _posts/C++/usexxx.cpp
+++ _posts/C++/usexxx.cpp
@@ -1,39 +1,29 @@
-// palindrome.cpp
+// rq17-8.cpp
 
 #include <iostream>
-#include <string>
-using namespace std;
-
-bool is_palindrome(string & str);
 
 int main()
 {
-	string str;
+	using namespace std;
 
-	cout << "문자열을 입력하세요 : ";
-	getline(cin, str);
+	char ch;
+	int ct1 = 0;
 
-	if (is_palindrome(str))
-		cout << "화문입니다.\n";
-	else
-		cout << "화문이 아닙니다.\n";
+	cin >> ch;
+	while (ch != 'q')
+	{
+		ct1++;
+		cin >> ch;
+	}
+
+	int ct2 = 0;
+	cin.get(ch);
+	while(ch != 'q')
+	{
+		ct2++;
+		cin.get(ch);
+	}
+	cout << "ct1 = " << ct1 << "; ct2 = " << ct2 << "\n";
 
 	return 0;
-}
-
-bool is_palindrome(string & str)
-{
-	for (auto i = str.begin(); i != str.end(); ++i)
-	{
-		if (!isalpha(*i))
-			str.erase(i--);
-		else
-			*i = tolower(*i);
-	}
-	for (auto f = str.begin(), r = str.end(); f < r; )
-	{
-		if (*f++ != *--r)
-			return false;
-	}
-	return true;
 }
